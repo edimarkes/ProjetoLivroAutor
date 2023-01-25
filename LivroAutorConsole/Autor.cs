4bbf5b3b6545f02d651ed796248f604f26bd5f9d
edimarkes <63433338+edimarkes@users.noreply.github.com>
2023-01-24 22:50:58 -0300

    version 0.1

---
+++ LivroAutorConsole/Autor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivroAutorConsole
+{
+    internal class Autor
+    {
+        public int AutorId { get; set; }
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+        public string Pais { get; set; }
+        public ICollection<LivroAutor> LivrosAutores { get; set; }
+
+    }
+}
